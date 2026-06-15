928a2ef4165986f7015c8b4f2d91aeae076a5c9c
nagasai <chodagamnagasai1@gmail.com>
2026-06-15 15:46:44 +0530

    Initalized repo

---
+++ guess game.py
@@ -0,0 +1,33 @@
+import random
+
+words = ("acid","ball","bake","cake","coin","dark","date","earn","ears","face","fake","fact","fear","fart","fast","fair","fail","gang","hall","icon","idle","jaws","jeep","lace","laid","lake","lamb","lame","lamp","land","made","maze","maid","mail","make","male","mall","many","milk","name")
+word = random.choice(words)
+guess_word = ""
+guess = 0
+guess_limit = 10
+out_of_guesses = False
+while not(out_of_guesses) and guess_word != word:
+    if guess < guess_limit:
+        guess_word = input("Guess the word: ")
+        guess += 1
+        if guess_word != word:
+            print("Wrong guess! Please try again.")
+
+            # Show matching letters
+            hint = ""
+            for i in range(len(word)):
+                if i < len(guess_word) and guess_word[i] == word[i]:
+                    hint += word[i] + " "
+                else:
+                    hint += "_ "
+
+            print("Hint:", hint)
+
+    else:
+        out_of_guesses = True
+        hint = ""
+if out_of_guesses:
+    print("You have failed to guess the correct word!")
+else:
+    print("You have guessed correctly!")
+    print(f"Number of guesses are {guess}")
